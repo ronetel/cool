9f0f694e643ceeb8280a88be0eebfbd911e6ffe3
ronetel <144216054+ronetel@users.noreply.github.com>
2023-11-07 22:43:40 +0300

    6 практос

---
+++ пр 6/пр 6/Program.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System.Xml.Serialization;
+using System.IO;
+using пр_6;
+
+Console.WriteLine("Добро пожаловать в текстовый редактор!");
+Console.Write("Введите путь к файлу: ");
+string filePath = Console.ReadLine();
+
+if (!File.Exists(filePath))
+{
+    Console.WriteLine("Файл не найден.");
+    return;
+}
+
+
+Figure figure = new Figure();
+
+if (filePath.EndsWith(".txt"))
+{
+
+    figure = Figure.ReadTxtFile(filePath);
+
+}
+else if (filePath.EndsWith(".json"))
+{
+    figure = Figure.ReadJsonFile(filePath);
+
+}
+else if (filePath.EndsWith(".xml"))
+{
+    figure = Figure.ReadXmlFile(filePath);
+}
+else
+{
+    Console.WriteLine("Формат файла не поддерживается.");
+
+}
+
+Console.WriteLine("Прочитанные данные:");
+Console.WriteLine("  Название:" + figure.Name);
+Console.WriteLine("  Ширина:" + figure.Width);
+Console.WriteLine("  Высота: " + figure.Height);
+ConsoleKeyInfo keyInfo;
+do
+{
+    keyInfo = Console.ReadKey();
+    if (keyInfo.Key == ConsoleKey.F1)
+    {
+        Console.Clear();
+        Console.WriteLine("Введите путь куда нужно сохранить файл");
+        Console.WriteLine("-------------------------------------");
+        string file = Console.ReadLine();
+
+        if (file.EndsWith(".txt"))
+        {
+
+            Figure.SaveTxtFile(figure, file);
+            break;
+        }
+        else if (file.EndsWith(".json"))
+        {
+            Figure.SaveJsonFile(figure, file);
+            break;
+        }
+        else if (file.EndsWith(".xml"))
+        {
+            Figure.SaveXmlFile(figure, file);
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Формат файла не поддерживается.");
+
+        }
+    }
+} while (keyInfo.Key != ConsoleKey.Escape);
+
---
+++ пр 6/пр 6/pop.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace пр_6
+{
+
+    public class Figure
+    {
+        public string Name;
+        public double Width;
+        public double Height;
+
+        public static Figure ReadTxtFile(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            if (lines.Length < 3)
+                throw new Exception("Некорректный формат файла.");
+
+            return new Figure
+            {
+                Name = lines[0],
+                Width = double.Parse(lines[1]),
+                Height = double.Parse(lines[2])
+            };
+        }
+
+        public static Figure ReadJsonFile(string filePath)
+        {
+            string jsonContent = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<Figure>(jsonContent);
+        }
+
+        public static Figure ReadXmlFile(string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Figure));
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                return (Figure)serializer.Deserialize(reader);
+            }
+        }
+
+        public static void SaveTxtFile(Figure figure, string file)
+        {
+            string[] lines = { figure.Name, figure.Width.ToString(), figure.Height.ToString() };
+            File.WriteAllLines(file, lines);
+            Console.WriteLine("Файл успешно сохранен в формате txt.");
+        }
+
+        public static void SaveJsonFile(Figure figure, string file)
+        {
+            string jsonContent = JsonConvert.SerializeObject(figure, Formatting.Indented);
+            File.WriteAllText(file, jsonContent);
+            Console.WriteLine("Файл успешно сохранен в формате json.");
+        }
+
+        public static void SaveXmlFile(Figure figure, string file)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Figure));
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                serializer.Serialize(writer, figure);
+            }
+            Console.WriteLine("Файл успешно сохранен в формате xml.");
+        }
+
+    }
+}
